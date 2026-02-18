c7fe902b26a51ef51ec7b0369237cd8ddc58da48
Hunain <hunainmazhariqbal@gmail.com>
2026-02-18 11:48:53 +0500

    @

---
+++ src/component/Update2.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const UpdateLearn2 = () => {
+  return (
+    <div><h1>
+        Muhammad Maqbool full stack devlooper
+      
+
+        </h1>
+        <h1>
+           hello my name is Hunain Mazhar
+        </h1>
+        <h1>
+           I update  my name in this project
+        </h1>
+        </div>
+  )
+}
+
+export default UpdateLearn2 ;
